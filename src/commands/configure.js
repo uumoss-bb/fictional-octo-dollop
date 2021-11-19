30990234a854e811ddcfa13f8e6bd347473870b8
bb_personal_git <balserbrodie@gmail.com>
2021-11-18 17:27:28 -0700

    update location for downloading sounds

--- src/commands/configure.js
+++ src/commands/configure.js
@@ -89,7 +89,7 @@ class ConfigureCommand extends Command {
   }
 
   async downloadSound() {
-    await fs.copy(`${this.rootLocation}/src/sounds/mixkit-scanning-sci-fi-alarm-905.wav`, `${this.config.configDir}/mixkit-scanning-sci-fi-alarm-905.wav`)
+    await fs.copy(`${this.rootLocation}/@londelo/pomo_go/src/sounds/mixkit-scanning-sci-fi-alarm-905.wav`, `${this.config.configDir}/mixkit-scanning-sci-fi-alarm-905.wav`)
     .then(() => console.log('success!'))
     .catch(err => console.error(err))
   }
